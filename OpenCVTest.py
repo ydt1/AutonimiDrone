a2b1877cd94390b6afdd06c2c14d38cde243c36f
razst <raz_st@hotmail.com>
2019-12-03 19:27:22 +0200

    added a working video stream option

--- OpenCVTest.py
+++ OpenCVTest.py
@@ -3,20 +3,33 @@
 import cv2
 import numpy as np
 from PIL import Image
+import threading
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from SocketServer import ThreadingMixIn
 import StringIO
+import time
 
 cap=None
+img=None
 FRAME_WIDTH=320
 FRAME_HEIGHT=240
+CAM_ID=1 # on laptop 0 for built-in cam, 1 for USB
+SHOW_WINDOWS=False
+
+
+H_MIN=0
+H_MAX=0
+S_MIN=0
+S_MAX=0
+V_MIN=255
+V_MAX=255
 
 def nothing(x):
     pass
 
 def setupCaptureObject():
     global cap
-    cap = cv2.VideoCapture(0)# select the roght webcam to use (on a laptop 0=built-in webcam, 1=USB webcam)
+    cap = cv2.VideoCapture(CAM_ID)# select the right webcam to use (on a laptop 0=built-in webcam, 1=USB webcam)
 
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
@@ -39,16 +52,20 @@ def setupHSVTrackBars():
 
 def readFromCam():
     kernel = np.ones((5, 5), np.uint8)
-
+    global img
     while True:
         ret, img = cap.read()
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # define range of HSV
-        HSV_MIN = np.array([cv2.getTrackbarPos('H Min', 'Trackbar'), cv2.getTrackbarPos(
-            'S Min', 'Trackbar'), cv2.getTrackbarPos('V Min', 'Trackbar')])
-        HSV_MAX = np.array([cv2.getTrackbarPos('H Max', 'Trackbar'), cv2.getTrackbarPos(
-            'S Max', 'Trackbar'), cv2.getTrackbarPos('V Max', 'Trackbar')])
+        if SHOW_WINDOWS:        
+            HSV_MIN = np.array([cv2.getTrackbarPos('H Min', 'Trackbar'), cv2.getTrackbarPos(
+                'S Min', 'Trackbar'), cv2.getTrackbarPos('V Min', 'Trackbar')])
+            HSV_MAX = np.array([cv2.getTrackbarPos('H Max', 'Trackbar'), cv2.getTrackbarPos(
+                'S Max', 'Trackbar'), cv2.getTrackbarPos('V Max', 'Trackbar')])
+        else:
+            HSV_MIN = np.array([H_MIN,S_MIN ,V_MIN ])
+            HSV_MAX = np.array([H_MAX,S_MAX ,V_MAX ])
 
         # Threshold the HSV image what was defined in the trackbars
         threshold = cv2.inRange(hsv, HSV_MIN, HSV_MAX)
@@ -74,16 +91,78 @@ def readFromCam():
             
             print(cx, cy)
 
-        cv2.imshow("image", img)
-        cv2.imshow("threshold", closing)
+        if SHOW_WINDOWS:
+            cv2.imshow("image", img)
+            cv2.imshow("threshold", closing)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+
+class CamHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		if self.path.endswith('.mjpg'):
+			self.send_response(200)
+			self.send_header('Content-type','multipart/x-mixed-replace; boundary=--jpgboundary')
+			self.end_headers()
+			while True:
+				try:
+					'''
+                    rc,img = capture.read()
+					if not rc:
+						continue
+					'''
+					imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+					jpg = Image.fromarray(imgRGB)
+					tmpFile = StringIO.StringIO()
+					jpg.save(tmpFile,'JPEG')
+					
+					self.wfile.write("--jpgboundary")
+					self.send_header('Content-type','image/jpeg')
+					self.send_header('Content-length',str(tmpFile.len))
+					self.end_headers()
+					
+					self.wfile.write( tmpFile.getvalue() )#jpg.save(self.wfile,'JPEG')
+					
+					time.sleep(0.05)
+				except KeyboardInterrupt:
+					
+					break
+			return
+		if self.path.endswith('.html'):
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.wfile.write('<html><head></head><body>')
+			self.wfile.write('<img src="http://127.0.0.1:8080/cam.mjpg"/>')
+			self.wfile.write('</body></html>')
+			return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+	"""Handle requests in a separate thread."""
+
+
+
+
 def main():
     setupCaptureObject()
-    setupHSVTrackBars()
-    readFromCam()
+    if SHOW_WINDOWS:
+        setupHSVTrackBars()
+        readFromCam()
+    else:
+        x = threading.Thread(target=readFromCam)
+        x.start()    
+        try:
+            server = ThreadedHTTPServer(('', 8080), CamHandler)
+            print "server started"
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print "about to exit.."
+            cap.release()
+            server.socket.close()
+            quit()
+
 
     # done reading from Camera, close all windows
     cv2.destroyAllWindows()
