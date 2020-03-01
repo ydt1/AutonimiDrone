a4dd4fdc8201b968a62a47f93a5713042724ae61
Raz <raz_st@hotmail.com>
2020-03-01 15:30:46 +0200

    fix

--- OpenCVTest.py
+++ OpenCVTest.py
@@ -11,7 +11,7 @@ import imutils
 outputFrame = None
 app = Flask(__name__)
 
-cap = cv2.VideoCapture(1)# select the roght webcam to use (on a laptop 0=built-in webcam, 1=USB webcam)
+cap = cv2.VideoCapture(0)# select the roght webcam to use (on a laptop 0=built-in webcam, 1=USB webcam)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
@@ -19,11 +19,13 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)  # disable auto focus
 # if using LED lights than disable exposure and brightness
 cap.set(cv2.CAP_PROP_EXPOSURE, -100)
-cap.set(cv2.CAP_PROP_BRIGHTNESS, -1)
+cap.set(cv2.CAP_PROP_BRIGHTNESS, -100)
 
 cap.set(cv2.CAP_PROP_FPS, 30)  # set FPS 30
 
-cv2.namedWindow('Trackbar')
+cv2.namedWindow('Settings')
+
+max_pixel = 320 * 240
 
 
 @app.route("/")
@@ -35,28 +37,26 @@ def nothing(x):
     pass
 
 
-cv2.createTrackbar('H Min', 'Trackbar', 58, 255, nothing)
-cv2.createTrackbar('H Max', 'Trackbar', 97, 255, nothing)
+cv2.createTrackbar('H Min', 'Settings', 60, 255, nothing)
+cv2.createTrackbar('H Max', 'Settings', 80, 255, nothing)
 
-cv2.createTrackbar('S Min', 'Trackbar', 13, 255, nothing)
-cv2.createTrackbar('S Max', 'Trackbar', 230, 255, nothing)
+cv2.createTrackbar('S Min', 'Settings', 0, 255, nothing)
+cv2.createTrackbar('S Max', 'Settings', 255, 255, nothing)
 
-cv2.createTrackbar('V Min', 'Trackbar', 105, 255, nothing)
-cv2.createTrackbar('V Max', 'Trackbar', 255, 255, nothing)
-
+cv2.createTrackbar('V Min', 'Settings', 0, 255, nothing)
+cv2.createTrackbar('V Max', 'Settings', 255, 255, nothing)
 
 kernel = np.ones((5, 5), np.uint8)
-
 
 while True:
     ret, img = cap.read()
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # define range of HSV
-    HSV_MIN = np.array([cv2.getTrackbarPos('H Min', 'Trackbar'), cv2.getTrackbarPos(
-        'S Min', 'Trackbar'), cv2.getTrackbarPos('V Min', 'Trackbar')])
-    HSV_MAX = np.array([cv2.getTrackbarPos('H Max', 'Trackbar'), cv2.getTrackbarPos(
-        'S Max', 'Trackbar'), cv2.getTrackbarPos('V Max', 'Trackbar')])
+    HSV_MIN = np.array([cv2.getTrackbarPos('H Min', 'Settings'), cv2.getTrackbarPos(
+        'S Min', 'Settings'), cv2.getTrackbarPos('V Min', 'Settings')])
+    HSV_MAX = np.array([cv2.getTrackbarPos('H Max', 'Settings'), cv2.getTrackbarPos(
+        'S Max', 'Settings'), cv2.getTrackbarPos('V Max', 'Settings')])
 
     # Threshold the HSV image what was defined in the trackbars
     threshold = cv2.inRange(hsv, HSV_MIN, HSV_MAX)
@@ -74,18 +74,30 @@ while True:
     # for contour in contours:
     #     cv2.drawContours(img, contour, -1, (0, 255, 0), 3)
 
-    contours, _ = cv2.findContours(closing, 1, 2)
-    if len(contours) > 0 :
-        cv2.drawContours(img, contours, -1, (0,255,0), 3)
+    _ , contours, _ = cv2.findContours(closing, 1, 2)
+    filter_contours = []
 
-        cnt = contours[0]
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        perimeter = cv2.arcLength(cnt,True)
+        print perimeter
+        if area < 250 or area > 22500   :
+            break
+        if perimeter < 10 or perimeter > 780:
+            break    
+        filter_contours.append(cnt)
+
+    if len(filter_contours) > 0 :
+        cv2.drawContours(img, filter_contours, -1, (0,255,0), 3)
+
+        cnt = filter_contours[0]
         
         M = cv2.moments(cnt)
 
         cx = int(160 - M['m10']/M['m00'])
         cy = int(120 - M['m01']/M['m00'])
         
-        print(cx, cy)
+        # print(cx, cy)
 
     cv2.imshow("image", img)
     cv2.imshow("threshold", closing)
