7270e6f6ea6140f6b5a3cdb1cf5255164ad171ff
yego163 <55788331+yego163@users.noreply.github.com>
2020-12-29 23:20:45 +0200

    T265 config And Get Position in python file with realsense2 lib

---
+++ T265_configAndGetPosition.py
@@ -0,0 +1,26 @@
+import pyrealsense2 as rs
+
+# Declare RealSense pipeline, encapsulating the actual device and sensors
+pipe = rs.pipeline()
+
+# Build config object and request pose data
+cfg = rs.config()
+cfg.enable_stream(rs.stream.pose)
+
+# Start streaming with requested config
+pipe.start(cfg)
+
+try:
+    for x in range(50):
+        # Wait for the next set of frames from the camera
+        frames = pipe.wait_for_frames()
+
+        # Fetch pose frame
+        pose = frames.get_pose_frame()
+        if pose:
+            # Print some of the pose data to the terminal
+            data = pose.get_pose_data()
+            print("Frame #{}".format(pose.frame_number))
+            print("Position: {}".format(data.translation))
+finally:
+    pipe.stop()
