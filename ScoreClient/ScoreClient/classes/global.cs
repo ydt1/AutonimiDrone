96ae451a1aed7c37c09751e0ea08ea80fea871e3
Raz <raz_st@hotmail.com>
2021-05-12 15:53:11 +0300

    score client c#

---
+++ ScoreClient/ScoreClient/classes/global.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ScoreClient.classes
+{
+    public class G//global
+    {
+        public static TcpClient socket;
+        public static NetworkStream clientStream;
+        public static string myName;
+        ~G()
+        {
+            try
+            {
+                socket.Close();
+            }
+            catch { }
+        }
+    }
+}
