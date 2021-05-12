44da09f7021de413aeacb3a4a7ff414e64cc3fdf
Raz <raz_st@hotmail.com>
2021-05-12 14:27:53 +0300

    score page with c#

---
+++ score/score/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Linq.Expressions;
+using System.Threading;
+using WindowsFormsApp1.classes;
+
+namespace Client
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            G.socket = new TcpClient();
+            if (!connect(G.socket))//if this failed
+                return;
+            G.clientStream = G.socket.GetStream();
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new WindowsFormsApp1.Form1());
+                send.start();
+            }
+            catch
+            {
+                List<Form> openForms = new List<Form>();
+
+                foreach (Form f in Application.OpenForms)
+                    openForms.Add(f);
+
+                foreach (Form f in openForms)//close all the windows exepte main
+                {
+                    if (f.Name != "Menu")
+                        f.Close();
+                }
+            }//catch an error = close the program
+            finally
+            {
+                send.logOut();
+            }
+        }
+        static bool connect(TcpClient client)
+        {
+            try
+            {
+                G.socket.Connect(new IPEndPoint(IPAddress.Parse("192.168.137.231"), 65432));//create the socket
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Server is down!");
+            }
+            return false;
+        }
+    }
+}
---
+++ score/score/classes/send.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.classes
+{
+    public static class send
+    {
+        private static string getStringFromServer()
+        {
+            byte[] buffer = new byte[5], data = new byte[4096];
+            int bytesRead = G.clientStream.Read(buffer, 0, 5);
+            int byteRead = G.clientStream.Read(data, 0, int.Parse(System.Text.Encoding.UTF8.GetString(buffer).Substring(1)));
+            return System.Text.Encoding.UTF8.GetString(buffer) + System.Text.Encoding.UTF8.GetString(data);//get the string
+        }
+        public static void start()
+        {
+            Byte[] sendBytes = Encoding.UTF8.GetBytes("Is anybody there?");
+            G.clientStream.Write(sendBytes, 0, sendBytes.Length);
+            G.clientStream.Flush();
+            // Reads NetworkStream into a byte buffer.
+            byte[] bytes = new byte[G.socket.ReceiveBufferSize];
+
+            // Read can return anything from 0 to numBytesToRead.
+            // This method blocks until at least one byte is read.
+            G.clientStream.Read(bytes, 0, (int)G.socket.ReceiveBufferSize);
+
+            // Returns the data received from the host to the console.
+            string returndata = Encoding.UTF8.GetString(bytes);
+
+            Console.WriteLine("This is what the host returned to you: " + returndata);
+        }
+        public static void logOut()
+        {
+            byte[] buffer = new ASCIIEncoding().GetBytes("0000");//logout
+            G.clientStream.Write(buffer, 0, buffer.Length);//send
+            G.clientStream.Flush();
+            getStringFromServer();
+            G.socket.Close();
+        }
+    }      
+}
